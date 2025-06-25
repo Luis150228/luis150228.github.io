27377661f8069e00e5ec3155cf010bbb8052025d
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-06-25 07:23:50 -0600

    Update: moficacion en el site de astro config

--- src/scripts/initNameDev.js
+++
@@ -1,6 +0,0 @@
-// src/scripts/initNameDev.js
-import { strokeLettersAnimated } from '../components/scripts/namedev-controller.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-	strokeLettersAnimated(0.3);
-});
