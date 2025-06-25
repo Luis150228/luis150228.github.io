360bf775927940509aea59d771ce4177111b290b
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-06-25 07:32:45 -0600

    Se modifica el astro config: site se ajusta https://luis150228.github.io/rangeldev.github.io/ junto con el base /rangeldev.github.io/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	site: 'https://luis150228.github.io/luis150228.github.io/',
-	base: '/luis150228.github.io/',
+	site: 'https://luis150228.github.io/rangeldev.github.io/',
+	base: '/rangeldev.github.io/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
