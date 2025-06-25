87d07974735f1caefb719ff395753e5939cec012
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-06-25 08:19:51 -0600

    ajuste en site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	site: 'https://luis150228.github.io/luis150228.github.io/',
+	site: 'https://luis150228.github.io/',
 	base: '/luis150228.github.io/',
 	vite: {
 		plugins: [tailwindcss()],
