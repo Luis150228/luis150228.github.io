2338fa5834a545743d5bf38365c878935d3ccc70
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-06-24 19:35:04 -0600

    Update: se agrega el base al astro config quitando el base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	base: '/rangeldev.github.io/',
+	site: 'https://rangeldev.github.io',
 	vite: {
 		plugins: [tailwindcss()],
 	},
