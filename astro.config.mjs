0e0772d4de04f6fca4afde61770e4e46846824cc
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-07-01 14:50:34 -0600

    Se agrega REACT y primer video asi como apertura de nueva ventana para link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 export default defineConfig({
 	site: 'https://luis150228.github.io/',
@@ -8,4 +9,5 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	integrations: [react()],
 });
